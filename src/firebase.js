f2c04e863f07febe6b0026474e9e98fe8d0d6f87
thinhphungdev <phungthinh0946519356@gmail.com>
2021-07-28 21:27:32 +0700

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectId: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
+  apiKey: "AIzaSyDKeI0l_l0ZisKReL4a477ZvofNxQ6hGXs",
+  authDomain: "https://tp-disney-clone.web.app/",
+  projectId: "tp-disney-clone",
+  storageBucket: "https://tp-disney-clone.appspot.com/",
+  messagingSenderId: "38936321497",
   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
   measurementId: "G-DRVLJKWRWG",
 };
